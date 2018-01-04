16b04aad6d292acaedafebc59cd879caef766aba
root <root@localhost.localdomain>
2018-01-04 22:58:22 +0000

    router.phpでindexページとsingleページを分岐

--- app/config/router.php
+++ app/config/router.php
@@ -2,6 +2,26 @@
 
 $router = $di->getRouter();
 
-// Define your routes here
+/**
+ * 民泊情報取得
+ * http://192.168.70.10
+ * GETでアクセス
+ * PostsControllerのindexメソッドが実行
+ */
+$router->addGet(
+    '/',
+    'Posts::index'
+);
+
+/**
+ * 民泊単体情報取得
+ * http://192.168.70.10/{id}
+ * GETでアクセス
+ * PostsControllerのsingleメソッドが実行
+ */
+$router->addGet(
+    '/{id:[0-9]+}',
+    'Posts::single'
+);
 
 $router->handle();
